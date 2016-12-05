dc21932096119815f3ccc9cbea8b7f172e98f98f
Chris-Sexton <champlain2013@gmail.com>
2016-12-05 14:14:59 -0500

    Updated models and views files.

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,6 @@
 class Rating < ActiveRecord::Base
   belongs_to :user
   belongs_to :recipe
+
+  validates :rating, numericality: {less_than_or_equal_to: 5}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
+  has_many :comments
+  has_many :recipes
+  has_many :ratings       
+
   def self.from_omniauth(auth)  
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
