4b6d7c8b1f39d0b4cc755309f3820d226730d061
Chris-Sexton <champlain2013@gmail.com>
2016-12-09 09:51:15 -0500

    Updated controllers and models.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,4 +3,10 @@ class Ingredient < ActiveRecord::Base
   has_many :recipes, through: :recipe_ingredients
 
   validates :name, presence: true
+
+  ef quantity(recipe)
+    q = RecipeIngredient.find_by(:recipe_id => recipe, :ingredient_id => self.id)
+    return q.quantity
+  end
+  
 end
