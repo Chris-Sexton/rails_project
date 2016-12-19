e3eaddeb35698b863ba6ea05300a15c9a5b87102
Chris-Sexton <champlain2013@gmail.com>
2016-12-19 10:19:29 -0500

    Updated quantity form.

--- app/controllers/recipe_ingredients_controller.rb
+++ app/controllers/recipe_ingredients_controller.rb
@@ -8,23 +8,11 @@ class RecipeIngredientsController < ApplicationController
   def create
     @recipe = Recipe.find(params[:recipe_id])
     @recipe_ingredients = @recipe.recipe_ingredients
-    @amount = params["/recipe_ingredients"].values[0].to_i
-    @ingredient_id = params.keys[2].to_i
 
-    setter = RecipeIngredient.set_amount(@recipe.id, @ingredient_id, @amount)
+    @ingredient_id = params["ingredient_id"]
+    @quantity = params["/recipe_ingredients"].values[0].to_i
+
+    setter = RecipeIngredient.set_amount(@recipe.id, @ingredient_id, @quantity)
     redirect_to recipe_path(@recipe)
   end
-
-  def update
-    @recipe = Recipe.find(params[:recipe_id])
-    @recipe_ingredients = @recipe.recipe_ingredients
-
-    @amount = params["/recipe_ingredients"].values[0].to_i
-    @ingredient_id = params['ingredient_id'].to_i
-
-    setter = RecipeIngredient.set_amount(@recipe.id, @ingredient_id, @amount)
-
-    redirect_to recipe_path(@recipe)
-  end
-
 end
